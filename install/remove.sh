3642cb5c1df9fab523e26adb33f45a7048611376
madhukartemba <97425422+madhukartemba@users.noreply.github.com>
2023-04-16 16:18:36 +0530

    updated remove script msgs

--- install/remove.sh
+++ install/remove.sh
@@ -7,11 +7,15 @@ INSTALL_DIR="/usr/local/bin"
 
 # Check if a previous version of SmartTest exists and remove it
 if [ -e "$INSTALL_DIR/SmartTest.run" ]; then
-  echo "Removing previous version of SmartTest..."
+  echo "Removing SmartTest..."
+  
   sudo rm "$INSTALL_DIR/SmartTest.run"
   sudo rm "$INSTALL_DIR/SmartTest"
+
+  # Print a message to the user indicating that the uninstallation is complete
+  echo "SmartTest has been uninstalled successfully!"
+else
+  echo "No previous version of SmartTest found."
 fi
 
-# Print a message to the user indicating that the uninstallation is complete
-echo "SmartTest has been uninstalled successfully!"
 
