38703e4867940f44637ac84d74f3e4bf9eb19987
madhukartemba <97425422+madhukartemba@users.noreply.github.com>
2023-04-16 16:15:40 +0530

    updated install and remove scripts

--- install/install.sh
+++ install/install.sh
@@ -2,15 +2,24 @@
 
 set -e
 
-# Copy the SmartTest.run file to /usr/local/bin/
-sudo cp SmartTest.run /usr/local/bin/
+# Define the installation directory
+INSTALL_DIR="/usr/local/bin"
 
-# Create a symbolic link from /usr/local/bin/SmartTest to /usr/local/bin/SmartTest.run
-sudo ln -s /usr/local/bin/SmartTest.run /usr/local/bin/SmartTest
+# Check if a previous version of SmartTest exists and remove it
+if [ -e "$INSTALL_DIR/SmartTest.run" ]; then
+  echo "Removing previous version of SmartTest..."
+  sudo rm "$INSTALL_DIR/SmartTest.run"
+  sudo rm "$INSTALL_DIR/SmartTest"
+fi
+
+# Copy the SmartTest.run file to the installation directory
+sudo cp SmartTest.run "$INSTALL_DIR/"
+
+# Create a symbolic link from SmartTest to SmartTest.run
+sudo ln -s "$INSTALL_DIR/SmartTest.run" "$INSTALL_DIR/SmartTest"
 
 # Set permissions on SmartTest.run to make it executable
-sudo chmod +rwx /usr/local/bin/SmartTest.run
+sudo chmod +x "$INSTALL_DIR/SmartTest.run"
 
 # Print a message to the user indicating that the installation is complete
 echo "SmartTest has been installed successfully!"
-
