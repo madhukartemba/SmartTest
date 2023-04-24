129218c5e136fc6679a43e8367e60b4936bae455
madhukartemba <madhusmiles.madhukar@gmail.com>
2023-04-24 15:16:12 +0530

    added comments

--- src/main/java/com/madhukartemba/smarttest/util/CodeParser.java
+++ src/main/java/com/madhukartemba/smarttest/util/CodeParser.java
@@ -4,13 +4,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CodeParser {
-
+    
+    // Regular expression pattern to match instances of a given keyword surrounded by non-alphanumeric characters
     Pattern pattern;
 
+    /**
+     * Constructs a CodeParser object with a regular expression pattern that matches instances of a given keyword 
+     * surrounded by non-alphanumeric characters.
+     * @param keyword the keyword to search for
+     */
     public CodeParser(String keyword) {
         pattern = Pattern.compile("[^a-zA-Z0-9_$]" + keyword + "[^a-zA-Z0-9_$]");
     }
 
+    /**
+     * Checks whether the input contains the keyword that was specified in the constructor.
+     * @param input the input string to search for the keyword in
+     * @return true if the input contains the keyword, false otherwise
+     */
     public boolean containsKeyword(String input) {
         Matcher matcher = pattern.matcher(input);
         return matcher.find();
